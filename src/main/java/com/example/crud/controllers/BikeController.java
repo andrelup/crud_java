74a6f2a889ee4cc44a8c52abb4f35e47424a70f7
André Pereira <andrelcop@gmail.com>
2025-03-21 12:49:37 +0100

    Short fixes

--- src/main/java/com/example/crud/controllers/BikeController.java
+++ src/main/java/com/example/crud/controllers/BikeController.java
@@ -12,19 +12,19 @@ import com.example.crud.model.Bike;
 import com.example.crud.services.BikeService;
 
 @RestController
-@RequestMapping("/cars")
+@RequestMapping("/bikes")
 public class BikeController {
 
     @Autowired
     private BikeService bikeService;
 
     @GetMapping("/")
-    public ResponseEntity<List<Bike>> getUsers() {
+    public ResponseEntity<List<Bike>> getBikes() {
         return ResponseEntity.ok(bikeService.getBikes());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Bike> getUserById(@PathVariable Long id) {
+    public ResponseEntity<Bike> getBikeById(@PathVariable Long id) {
         return ResponseEntity.ok(bikeService.getBikeById(id));
     }
 }
